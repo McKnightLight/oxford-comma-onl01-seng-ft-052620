6ad34799d20d79ce171388ac1707a2a6df3e9cac
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-28 20:05:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,11 @@ require 'pry'
 def oxford_comma(array)
   if array.length == 1 
     array[0]
-  elsif array.length == 2
+  elsif array.length == 3
   array [0] + " and " + array[1]
 else
   #binding.pry
-  array[-1] = "and"+array[-1]
+  array[-1] = "and"+ array[-1]
   array.join(", ")
   end
 end
