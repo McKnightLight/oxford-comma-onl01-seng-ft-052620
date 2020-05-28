51ce98d010a1a4a92eadcbdb539e40192d9112a9
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-28 19:59:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 def oxford_comma(array)
-  #binding.pry
   if array.length == 1 
     array[0]
   elsif array.length == 2
   array [-2] << " and " + array[1]
+else
+  binding.pry
   end 
 end
