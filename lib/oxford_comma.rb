26001439ed1f087b3baf0693091c283ba3945f42
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-28 19:59:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,22 +6,5 @@ def oxford_comma(array)
     array[0]
   elsif array.length == 2
   array [-2] << " and " + array[1]
-  array.join
-  elsif array.
   end 
 end
-
-array = ["kiwi"]
-def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
-end
-
-oxford_comma(array)
