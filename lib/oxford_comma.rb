8155a0de197ada2f31113430021292bf92a62baf
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-28 20:06:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
-require 'pry'
-
+array = ["cabbage"]
 def oxford_comma(array)
-  if array.length == 1 
-    array[0]
-  elsif array.length == 3
-  array [0] + " and " + array[1]
-else
-  #binding.pry
-  array[-1] = "and"+ array[-1]
-  array.join(", ")
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
   end
 end
